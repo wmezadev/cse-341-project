3d8f7898e2d3a62c65cda7a6ce8e49b218658435
William Eduardo Meza <williameduardomeza@gmail.com>
2022-11-17 00:52:17 -0500

    feat: add linting and formating

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +9,11 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World with TypeScript!');
+});
+
+app.use(bodyParser.json()).use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 app.listen(port, () => {
