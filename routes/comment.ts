aa28d60ff7418fac6b7c0f78d31274d5d31cba8e
William Eduardo Meza <williameduardomeza@gmail.com>
2022-11-20 00:56:25 -0500

    feat: use validator request in routes

--- routes/comment.ts
+++ routes/comment.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import { CommentController } from '../controllers';
+import { CommentStoreRequest, CommentUpdateRequest } from '../requests';
 
 export const commentRoutes: Router = express.Router();
 
 commentRoutes.get('/comment', CommentController.index);
-commentRoutes.post('/comment', CommentController.store);
+commentRoutes.post('/comment', CommentStoreRequest, CommentController.store);
 commentRoutes.get('/comment/getByPost/:post_id', CommentController.getByPost);
 commentRoutes.get('/comment/:id', CommentController.show);
 commentRoutes.put('/comment/:id', CommentController.udpate);
+commentRoutes.put('/comment/:id', CommentUpdateRequest, CommentController.udpate);
 commentRoutes.delete('/comment/:id', CommentController.destroy);
--- routes/post.ts
+++ routes/post.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
 import { PostController } from '../controllers';
+import { PostRequest } from '../requests';
 
 export const postRoutes: Router = express.Router();
 
 postRoutes.get('/post', PostController.index);
-postRoutes.post('/post', PostController.store);
+postRoutes.post('/post', PostRequest, PostController.store);
 postRoutes.get('/post/:id', PostController.show);
 postRoutes.put('/post/:id', PostController.update);
 postRoutes.delete('/post/:id', PostController.destroy);
